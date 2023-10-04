f9e6b721fef3ac405852c0a14c774394942612bc
htai67934@gmail.com <htai67923@gmail.com>
2023-10-04 14:57:11 +0700

    EP-21 Sản phẩm yêu thích

--- resources/views/Client/layout/Master.blade.php
+++ resources/views/Client/layout/Master.blade.php
@@ -1,34 +1,3 @@
-<!DOCTYPE html>
-<html class="no-js" lang="en">
-
-<head>
-    <meta charset="utf-8" />
-    <title>Nest - Multipurpose eCommerce HTML Template</title>
-    <meta http-equiv="x-ua-compatible" content="ie=edge" />
-    <meta name="description" content="" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <meta property="og:title" content="" />
-    <meta property="og:type" content="" />
-    <meta property="og:url" content="" />
-    <meta property="og:image" content="" />
-    <!-- Favicon -->
-    <link rel="shortcut icon" type="image/x-icon" href="{{asset('assets/imgs/theme/favicon.svg')}}" />
-    <!-- Template CSS -->
-    <link rel="stylesheet" href="{{asset('assets/css/plugins/animate.min.css')}}" />
-    <link rel="stylesheet" href="{{asset('assets/css/main.css?v=5.6')}}" />
-    {{--Account upload anh --}}
-    <link rel="stylesheet" href="{{asset('assets/css/style_blog3.css')}}">
-</head>
-<body>
-    @include('Client/Header/header');
-    <!-- Navbar End -->
-
-    <!-- Carousel Start -->
-    <div class="wapper-main">
-
-       @yield('main')
-    </div>
-    <!-- Contact Start -->
 
     <!-- Footer Start -->
     @include('Client/Footer/footer');
